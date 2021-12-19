577ef3e0dac0f7320ae02996187507b3000a3c83
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-12-19 16:15:19 +0530

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,24 +4,37 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import AddFarmer from './components/farmer/AddFarmer';
+import DeleteFarmer from './components/farmer/DeleteFarmer';
+import FarmerById from './components/farmer/FarmerById';
+import UpdateFarmer from './components/farmer/UpdateFarmer';
+import ViewAllFarmers from './components/farmer/ViewAllFarmers'
 
 import AddCustomer from './components/customer/AddCustomer';
 import GetAllCustomer from './components/customer/GetAllCustomers';
 import GetCustomerById from './components/customer/GetCustomerById';
 import DeleteCustomer from './components/customer/DeleteCustomer';
 import UpdateCustomer from './components/customer/UpdateCustomer';
+import CustomerPayBillToDealer from './components/customer/CustomerPayBillToDealer';
 
 import UpdateDealer from './components/dealer/UpdateDealer';
 import DeleteDealer from './components/dealer/DeleteDealer';
 import AddDealer from './components/dealer/AddDealer';
 import GetAllDealer from './components/dealer/GetAllDealer';
 import GetDealerById from './components/dealer/GetDealerById';
+import DealerSellMilkToCustomer from './components/dealer/DealerSellMilkToCustomer';
+import DealerPayBillToCompany from './components/dealer/DealerPayBillToCompany';
+import GetAllDealerSellOrders from './components/dealer/GetAllDealerSellOrders';
 
 import DeleteCompany from './components/company/DeleteCompany';
 import AddCompany from './components/company/AddCompany';
 import GetAllCompany from './components/company/GetAllCompany';
 import GetCompanyById from './components/company/GetCompanyById';
 import UpdateCompany from './components/company/UpdateCompany';
+import CompanyBuyMilkFromFarmer from './components/company/CompanyBuyMilkFromFarmer';
+import CompanySellsMilkToDealer from './components/company/CompanySellMilkToDealer';
+import CompanyPayBillToFarmer from './components/company/CompanyPayBillToFarmer';
+import GetAllCompanyBuyOrders from './components/company/GetAllCompanyBuyOrders';
 
 
 
@@ -83,6 +96,10 @@ test('render Data from Update New Customer', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
+
+
+
 //DealetTests------------------------------------------------------------------------------------------
 
 test('render Data from Update New Dealer', () => {
@@ -132,6 +149,34 @@ test('render Data from Find Dealer by id', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('render Data from  Dealer Sell Milk Transaction', () => {
+   
+  render(
+    <Provider store={store} >
+       <DealerSellMilkToCustomer/>
+    </Provider>)
+  const linkElement = screen.getByText('Dealer Sell Milk Transaction');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from  Dealer-Company Bill Payment', () => {
+   
+  render(
+    <Provider store={store} >
+       < DealerPayBillToCompany/>
+    </Provider>)
+  const linkElement = screen.getByText('Dealer-Company Bill Payment');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data from  Find All sell orders', () => {
+   
+  render(
+    <Provider store={store} >
+       <GetAllDealerSellOrders/>
+    </Provider>)
+  const linkElement = screen.getByText('Find All sell orders');
+  expect(linkElement).toBeInTheDocument();
+});
 //Company----------------------------------------------------------------------------------------
 test('render Data  Delete company by id', () => {
   render(
@@ -184,6 +229,92 @@ test('render Data from Update New Company', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('render Data from Company Buy Milk Transaction', () => {
+   
+  render(
+    <Provider store={store} >
+       <CompanyBuyMilkFromFarmer/>
+    </Provider>)
+  const linkElement = screen.getByText('Company Buy Milk Transaction');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from Company Sell Milk Transaction', () => {
+   
+  render(
+    <Provider store={store} >
+       <CompanySellsMilkToDealer/>
+    </Provider>)
+  const linkElement = screen.getByText('Company Sell Milk Transaction');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from Company-Farmer Bill Payment', () => {
+   
+  render(
+    <Provider store={store} >
+       <CompanyPayBillToFarmer/>
+    </Provider>)
+  const linkElement = screen.getByText('Company-Farmer Bill Payment');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data from Find All buy orders', () => {
+   
+  render(
+    <Provider store={store} >
+       <GetAllCompanyBuyOrders />
+    </Provider>)
+  const linkElement = screen.getByText('Find All buy orders');
+  expect(linkElement).toBeInTheDocument();
+});
+
+//Farmer test cases-------------------------------------------------------------
+test('render Data from Add New Farmer', () => {
+   
+  render(
+    <Provider store={store} >
+       <AddFarmer/>
+    </Provider>)
+  const linkElement = screen.getByText('Add New Farmer');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from Delete farmer by id', () => {
+   
+  render(
+    <Provider store={store} >
+       <DeleteFarmer/>
+    </Provider>)
+  const linkElement = screen.getByText('Delete farmer by id');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from Update New Farmer', () => {
+   
+  render(
+    <Provider store={store} >
+       <UpdateFarmer/>
+    </Provider>)
+  const linkElement = screen.getByText('Update New Farmer');
+  expect(linkElement).toBeInTheDocument();
+});
+test('render Data from List of all Farmers', () => {
+   
+  render(
+    <Provider store={store} >
+       <ViewAllFarmers/>
+    </Provider>)
+  const linkElement = screen.getByText('List of all Farmers');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data from Find farmer by id', () => {
+   
+  render(
+    <Provider store={store} >
+       <FarmerById/>
+    </Provider>)
+  const linkElement = screen.getByText('Find farmer by id');
+  expect(linkElement).toBeInTheDocument();
+});
+
 
 
 
@@ -193,5 +324,14 @@ test('render Data from Update New Company', () => {
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('render Data  from CustomerPayBillToDealer', () => {
+//   render(
+//     <Provider store={store} >
+//       <CustomerPayBillToDealer />
+//     </Provider>)
+//   const linkElement = screen.getByText('CustomerPayBillToDealer');
 //   expect(linkElement).toBeInTheDocument();
 // });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,66 @@
 const Home = () => {
-
     return (
-        <div>
-            <h1 className="display-4 text-primary mt3">Welcome to Capgemini</h1>
-            <p>Capgemini trainees, especially those who are from Oct 12 batch3 are really very genious!</p>
-            <div>
-                <img width="1080" src="https://www.teahub.io/photos/full/80-801743_full-hd-3d-plain-background.jpg" alt="Capgemini Office"></img>
-            </div>
-        </div>
+
+        <div className="container"
+            style={{ height: "90vh" }}
+        >
+            <h1 className="display-4 text-primary  pt-5"><h4>Welcome to</h4>Online Dairy Management</h1>
+            <p>As a part of Sprint-2 this</p>
+        </div> 
+       
+
+        
     );
 }
-
 export default Home;
+
+
+
+
+
+
+
+
+// const Home = () => {
+
+//     return (
+//         <div
+//             style={{
+
+//                 backgroundImage: "url(" + "https://media.istockphoto.com/photos/the-silhouette-of-a-farmer-stands-near-a-cow-milk-cans-in-the-picture-id1301384827?b=1&k=20&m=1301384827&s=170667a&w=0&h=FYOAq55S21fIWd1FZT_AS7WdbkDYIpWUGcGThhnVL0I=" + ")",
+//                 backgroundPosition: 'center',
+//                 backgroundSize: 'cover',
+//                 backgroundRepeat: 'no-repeat'
+
+//             }}>
+//             <h1 className="display-4 text-primary mt3">Welcome to Capgemini</h1>
+//             <p>Capgemini trainees, especially those who are from Oct 12 batch3 are really very genious!</p>
+
+//         </div>
+//     );
+// }
+
+// export default Home;
+
+
+//-------------------------------------------------------------------------------------------
+// <div className="container"
+
+        //     style={{
+        //         backgroundImage: "url(" + "https://media.istockphoto.com/photos/the-silhouette-of-a-farmer-stands-near-a-cow-milk-cans-in-the-picture-id1301384827?b=1&k=20&m=1301384827&s=170667a&w=0&h=FYOAq55S21fIWd1FZT_AS7WdbkDYIpWUGcGThhnVL0I=" + ")",
+        //         backgroundPosition: 'left center',
+        //         backgroundSize: 'cover',
+        //         backgroundRepeat: 'no-repeat'
+        //     }}>
+        //     <div className=".container-flluid"
+        //         style={{
+        //             height:500,
+                
+        //             color: "white"
+        //         }}
+        //     >
+        //         <h1 className="display-4 text-primary m-1 pt-5">Welcome to Dairy Management</h1>
+        //         <p>Dairy Management Online</p>
+        //     </div>
+
+        // </div>
